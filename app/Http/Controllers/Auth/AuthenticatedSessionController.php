295f817b2744712721005c495a1fc0ffa1889b83
bouqrt <bouchraelqrt@gmail.com>
2026-04-07 16:13:03 +0100

    feat: role-based redirection after login

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -28,7 +28,15 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('dashboard', absolute: false));
+        $user = auth()-> user();
+
+        if ($user->role === 'admin'){
+            return redirect('/admin/dashboard');
+        } elseif ($user->role ==='entreprise'){
+            return redirect('/entreprise/dashboard');
+        } else {
+            return redirect('/student/dashboard');
+        }
     }
 
     /**
