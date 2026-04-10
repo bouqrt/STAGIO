584eb2c1259bf6e7663a5295bf4526168c7114a2
bouqrt <bouchraelqrt@gmail.com>
2026-04-11 00:44:33 +0100

    feat: implement entreprise creation logic

--- app/Http/Controllers/EntrepriseController.php
+++ app/Http/Controllers/EntrepriseController.php
@@ -3,9 +3,28 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Entreprise;
 use Illuminate\Http\Request;
 
 class EntrepriseController extends Controller
 {
-    //
+    public function create ()
+    {
+        return view('entreprise.create');
+    }
+
+    public function store(request $request)
+    {
+        Entreprise::create([
+            'user_id' => auth()->id(),
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'address' => $request->address,
+            'description' => $request->description,
+    ]);
+
+    return redirect('/entreprise/dashboard');
+    
+    }
 }
--- app/Models/Entreprise.php
+++ app/Models/Entreprise.php
@@ -5,7 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class Entreprise extends Model
-{
+{   
+    protected $fillable = [
+    'user_id',
+    'name',
+    'email',
+    'phone',
+    'address',
+    'description'
+    ];
     public function user()
     {
         return $this->belongsTo(User::class);
