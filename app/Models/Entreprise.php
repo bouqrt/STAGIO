908a16eebb4088c3c9ef603a33b634eef572bfdc
bouqrt <bouchraelqrt@gmail.com>
2026-04-15 20:51:05 +0100

    feat: add relation between entreprise and offres

--- app/Models/Entreprise.php
+++ app/Models/Entreprise.php
@@ -18,4 +18,9 @@ class Entreprise extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function offres()
+    {
+    return $this->hasMany(Offre::class);
+    }
 }
