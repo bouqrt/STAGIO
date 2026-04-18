57326ca5b479f7a44ec2ac108a9f8f7a65932fbf
bouqrt <bouchraelqrt@gmail.com>
2026-04-18 18:49:15 +0100

    feat: implement candidature creation

--- app/Http/Controllers/CandidatureController.php
+++ app/Http/Controllers/CandidatureController.php
@@ -3,9 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Candidature;
+use App\Models\Offre;
 use Illuminate\Http\Request;
 
 class CandidatureController extends Controller
 {
-    //
+    public function store(Request $request, $id)
+{
+    $offre = Offre::findOrFail($id);
+
+    Candidature::create([
+        'user_id' => auth()->id(),
+        'offre_id' => $offre->id,
+    ]);
+
+    return back()->with('success', 'Candidature envoyée');
+}
 }
--- app/Models/Candidature.php
+++ app/Models/Candidature.php
@@ -6,6 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Candidature extends Model
 {
+    protected $fillable = [
+        'user_id',
+        'offre_id',
+        'cv',
+        'status'
+    ];
     public function user()
     {
         return $this->belongsTo(User::class);
