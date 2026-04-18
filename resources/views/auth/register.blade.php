2c0e8510ddaca9cb9326fce6cebb36a9c3f85d8f
bouqrt <bouchraelqrt@gmail.com>
2026-04-18 18:06:35 +0100

    feat: add role selection in register form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -16,6 +16,14 @@
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
+        <div>
+            <label>Register as:</label>
+                <select name="role" required>
+                    <option value="student">Student</option>
+                    <option value="entreprise">Entreprise</option>
+                </select>
+        </div>
+
         <!-- Password -->
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
